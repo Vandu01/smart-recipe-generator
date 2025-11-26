3509b1e6632ab67d6c27054707f01811dd7d4a5b
vandana <vandanajnvalwar@gmail.com>
2025-11-27 01:36:45 +0530

    update

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,34 +3,37 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./database/db");
 
-
 // Load environment variables
-dotenv.config({ path:  "./config/.env" });
+dotenv.config({ path: "./config/.env" });
 
 // Connect MongoDB
 connectDB();
 
 const app = express();
 
-// middlewares
+
 app.use(cors({
-    origin: [
-      "http://localhost:5173",                
-      "https://spontaneous-pithivier-532b61.netlify.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  }));
+  origin: [
+    "http://localhost:5173",
+    "https://spontaneous-pithivier-532b61.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type"]
+}));
+
+// Handle preflight requests
+app.options("*", cors());
+
 app.use(express.json());
 
-// routes
+// Routes
 const recipeRoutes = require("./routes/recipeRoutes");
 app.use("/api/recipes", recipeRoutes);
 
-// default route
+// Default route
 app.get("/", (req, res) => {
   res.send("Smart Recipe Generator Backend Running...");
 });
 
 module.exports = app;
-
